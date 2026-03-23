7edff8b2e2380ccb5eba3793377913a6588bfe64
Guilherme Miranda <superaplicativos2022@gmail.com>
2026-03-23 15:22:59 -0300

    Torna clone 100% independente do domínio antigo

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -1,14 +1,14 @@
-<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+﻿<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://mobiler.com.br</homePageLink>
+		<homePageLink></homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://mobiler.com.br/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://mobiler.com.br/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://mobiler.com.br/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://mobiler.com.br/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://mobiler.com.br/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="/wp-json/" />
 			</apis>
 	</service>
 </rsd>
